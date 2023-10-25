dbeef4fd5c2046c00757c1d0b6886fcf8d527bf1
mmsharepoint <markus.moeller.ed@outlook.de>
2023-10-25 10:25:18 +0200

    Fix for deploy

--- src/components/FileNodeLabel.tsx
+++ src/components/FileNodeLabel.tsx
@@ -3,13 +3,8 @@ import { useDrag } from 'react-dnd';
 import { IFileNodeProps } from './IFileNodeProps';
 import './FileNode.css';
 
-export const FileNodeLabel: React.FC<IFileNodeProps> = (props) => {
-  // const drag = React.useCallback((ev: React.DragEvent<HTMLElement>)=> {
-  //   const transfer = { node: props.node, file: props.file, sourceNodeIndex: props.parentIndex, sourceParentIndex: props.grandParentIndex, fileIndex: props.fileIndex}
-  //   ev.dataTransfer.setData("text/plain", JSON.stringify(transfer));
-  // }, [props.file]);
-
-  const [{ isDragging }, drag] = useDrag(() => ({
+export const FileNodeLabel: React.FC<IFileNodeProps> = (props) => {  
+  const [ , drag] = useDrag(() => ({
     type: 'FileNodeLabel',
     item: { file: props.file, parent: props.node },
     collect: (monitor) => ({
